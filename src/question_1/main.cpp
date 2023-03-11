356b46312bb6a6d1613f6583cd5dea3362ab6723
RobinGTodd <31745329+RobinGTodd@users.noreply.github.com>
2023-03-11 00:25:31 +0000

    question1

--- src/question_1/main.cpp
+++ src/question_1/main.cpp
@@ -1,6 +1,26 @@
+#include<iostream>
+#include "question1.h"
+
+using std::cout; using std::cin;
 
 int main()
 {
+    double m;
+    double v;
+    char exit_condition = 'n';
+    cout<<"Kinetic Energy Calculator\n";
+    do 
+    {
+        cout<<"Enter a mass (kg)\n";
+        cin>>m;
+        cout<<"Enter a velocity (m/s)\n";
+        cin>>v;
+        cout<<"Kinetic Energy: "<<get_kinetic_energy(m, v)<<"\n";
+        cout<<"\n exit? (y/n)\n";
+        cin>>exit_condition;
+        cout<<"\n";
 
+    }
+    while(exit_condition != 'y');
     return 0;
 }
--- src/question_1/question1.cpp
+++ src/question_1/question1.cpp
@@ -1,6 +1,14 @@
 #include "question1.h"
+#include<iostream>;
 
 bool test_config()
 {
     return true;
+}
+
+double get_kinetic_energy(double m, double v)
+{
+    auto result = 0.5*m*v*v;
+    std::cout<<"result: "<<result<<"\n";
+    return result;
 }
