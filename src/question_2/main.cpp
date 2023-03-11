b7e328771faf58dc28095df242acc3de3127a59f
RobinGTodd <31745329+RobinGTodd@users.noreply.github.com>
2023-03-11 17:53:48 +0000

    Question1

--- src/question_2/main.cpp
+++ src/question_2/main.cpp
@@ -1,6 +1,12 @@
+#include "question2.h"
+#include<iostream>
+#include<string>
+
+using std::cout; using std::cin;
 
 int main()
 {
-
+    double result = get_dna_p_distance("GAGCCTACTAACGGGAT", "CATCGTAATGACGGCCT");
+    cout<<"Result: "<<result<<"\n";
     return 0;
 }
