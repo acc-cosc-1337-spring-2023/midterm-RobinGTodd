6bf10231341d43058dfce3f312e00be3eb63a954
RobinGTodd <31745329+RobinGTodd@users.noreply.github.com>
2023-03-11 20:17:50 +0000

    question4

--- src/question_4/main.cpp
+++ src/question_4/main.cpp
@@ -1,6 +1,21 @@
+#include "question4.h"
+#include<iostream>
+
+using std::cout; using std::cin;
 
 int main()
 {
+    char exit_condition = 'n';
+    do
+    {
+        int user_input = get_user_input();
+        cout<<"Result: "<<get_factorial_sequence(user_input)<<"\n";
+
+        cout<<"Exit? (y/n)\n";
+        cin>>exit_condition;
+        cout<<"\n\n";
+    }
+    while (exit_condition == 'n');
 
     return 0;
 }
